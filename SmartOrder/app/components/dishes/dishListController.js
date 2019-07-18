e8809fd01109e8e7450b12cd586d5891ac64635d
Chử Hoàng Anh <kukuanhanh@gmail.com>
2019-07-18 22:42:02 +0700

    update list dish add

--- SmartOrder/app/components/dishes/dishListController.js
+++ SmartOrder/app/components/dishes/dishListController.js
@@ -1,10 +1,11 @@
 ﻿(function (app) {
     app.controller('listDishController', listDishController);
 
-    listDishController.$inject = ['$scope', '$http', 'ngDialog','apiService'];
+    listDishController.$inject = ['$scope', 'ngDialog', 'apiService'];
 
-    function listDishController($scope, $http, ngDialog, apiService) {
-     
+    function listDishController($scope, ngDialog, apiService) {
+
+        //get data from api
         function getDish(index, pageSize, totalRow) {
             index = index || 0;
             pageSize = pageSize || 100;
@@ -12,31 +13,23 @@
 
             var config = {
                 params: {
-                    index: index ,
-                    pageSize: pageSize, 
-                     totalRow : totalRow,
+                    index: index,
+                    pageSize: pageSize,
+                    totalRow: totalRow,
                 }
             }
-
             apiService.get('/api/dish/getall', config, function (result) {
                 $scope.dishes = result.data.listDish;
-                console.log(result.data.listDish);
             }, function () {
                 console.log('Load dish failed.');
             });
         }
-
         getDish();
 
-
-
-
+        //show the dialog ditail dish by table
         var vm = this;
-
-        vm.openDialog = function ($event,item) {
-
+        vm.openDialog = function ($event, item) {
             var dish = item
-            
 
             vm.init = function () {
                 $scope.dish = dish;
@@ -44,7 +37,7 @@
 
             vm.init();
 
-            // Show the dialog in the main controller 
+            // Show the dialog in the main controller
             var dialog = ngDialog.open({
                 template: '/app/components/dishes/dishEditsView.html',
                 scope: $scope,
@@ -53,12 +46,18 @@
                 closeByDocument: false, //can not close dialog by click out of dialog area
                 className: 'ngdialog',
                 showClose: false,
-            });           
+            });
         };
 
+        //function sort by title tr tag
         $scope.sort = function (keyname) {
             $scope.sortKey = keyname;   //set the sortKey to the param passed
             $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+        };
+
+        //function for add button 
+        $scope.dishAdd = function () {
+            
         }
 
     }
