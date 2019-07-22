2a3d50da4ec6321fc103d1372ff1e2e542d0878f
Chử Hoàng Anh <kukuanhanh@gmail.com>
2019-07-22 22:45:05 +0700

    Prepare to pull

--- SmartOrder/app/components/restaurant/listBillController.js
+++ SmartOrder/app/components/restaurant/listBillController.js
@@ -1,7 +1,33 @@
 ﻿(function (app) {
     app.controller('listBillController', listBillController);
 
-    function listBillController() {
+    listBillController.$inject = ['$scope', 'ngDialog', 'apiService', 'notificationService'];
+
+    function listBillController($scope, ngDialog, apiService, notificationService) {
+
+        //for user customize date from to .. 
+        $scope.dateSeclect = {
+            from: new Date(),
+            to: new Date
+        };
+
+        var optionArr = new Array();
+        function tmp(key, val) {
+            this.key = key;
+            this.val = val;
+        }
+        var op = new tmp(1, 'Hôm nay');
+        optionArr.push(op);
+        op = new tmp(2, 'Tuần này');
+        optionArr.push(op);
+        op = new tmp(3, 'Tháng này');
+        optionArr.push(op);
+
+        $scope.options = optionArr;
+        $scope.userOption;
+
+
+            
 
     }
 })(angular.module('SmartOrder.restaurant'));
