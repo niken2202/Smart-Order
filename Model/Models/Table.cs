90cfb152f0897c37374edf030b7a411243c9d9c1
Le Trong Thang <niken2202@gmail.com>
2019-07-30 16:37:31 +0700

    update table

--- Model/Models/Table.cs
+++ Model/Models/Table.cs
@@ -7,13 +7,15 @@ namespace Model.Models
     [Table("Table")]
     public class Table
     {
-        [Key]
+        [Key,Column(Order =1)]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { set; get; }
 
         [Required]
         [MaxLength(256)]
         public string Name { set; get;}
+        [Key, Column(Order = 2)]
+        public string DeviceID { get; set; }
         public int Status { get; set; }
     }
 }
