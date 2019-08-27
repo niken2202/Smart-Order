39204316e029d87418a861263f8d42058f6eaba0
Le Trong Thang <niken2202@gmail.com>
2019-08-28 01:08:06 +0700

    update cart

--- Data/Repositories/CartRepository.cs
+++ Data/Repositories/CartRepository.cs
@@ -52,7 +52,7 @@ namespace Data.Repositories
                         select c).SingleOrDefault();
             if (cart == null) return null;
             cart.CartDetails = (from cd in DbContext.CartDetail
-                                where cd.CartID == cart.ID
+                                where cd.CartID == cart.ID && cd.Status!=0
                                 select cd).ToList();
             cart.CartPrice = cart.CartDetails.Sum(x => (x.Price * x.Quantity));
             return cart;
