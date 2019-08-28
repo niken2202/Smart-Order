32ff574b453db71ca88afee764f764a198e1d4b6
Chử Hoàng Anh <kukuanhanh@gmail.com>
2019-08-28 17:33:04 +0700

    update password account

--- SmartOrder/app/components/accounts/editsAccountController.js
+++ SmartOrder/app/components/accounts/editsAccountController.js
@@ -5,29 +5,28 @@
 
     function editsAccountController($scope, apiService, notificationService, $stateParams) {
         $scope.notification = "";
-        $scope.psw = "";
-        $scope.rePsw = "";
-        $scope.condition = false;
+        $scope.newPsw = "";
+        $scope.reNewPsw = "";
+        $scope.current = "";
         $scope.changePswCondition = false;
 
         $scope.changerPSW = changerPSW;
         function changerPSW() {
             if ($scope.changePswCondition == false) {
                 $scope.changePswCondition = true;
-                $scope.condition = true;
+                document.getElementById("divChangePassword").style.display = "block";
             } else {
                 $scope.changePswCondition = false;
+                document.getElementById("divChangePassword").style.display = "none";
             }
         }
 
         //check password and rePassword
         $scope.checkPSW = checkPSW;
         function checkPSW() {
-            if (angular.equals($scope.psw, $scope.rePsw) == true) {
-                $scope.condition = false;
+            if (angular.equals($scope.newPsw, $scope.reNewPsw) == true) {
                 $scope.notification = ""
             } else {
-                $scope.condition = true;
                 $scope.notification = "Mật khẩu không khớp!"
             }
         }
@@ -42,7 +41,7 @@
             });
         }
         getRoles();
-
+        
         //get account infomation by id
         function getComboByID() {
             var transfer = {
@@ -50,6 +49,7 @@
                     id: $stateParams.ID,
                 }
             }
+            document.getElementById("divChangePassword").style.display = "none";
             apiService.get('api/user/getbyid', transfer,
                 function (result) {
                     $scope.accEdt = result.data;
@@ -59,5 +59,48 @@
                 });
         }
         getComboByID();
+
+        $scope.updatePsw = updatePsw;
+        function updatePsw() {
+            if (angular.equals($scope.newPsw, $scope.reNewPsw) == true) {
+                var updatePsw = {
+                    Id: $scope.accEdt.Id,
+                    CurrentPassword: $scope.newPsw,
+                    NewPassword: $scope.current
+                }
+                apiService.post('/api/user/changepassword', updatePsw, function (result) {
+                    notificationService.displaySuccess('Thêm mới tài khoản thành công!');
+                }, function () {
+                    notificationService.displayError('Tạo mới tài khoản không thành công !');
+                });
+
+            }
+        }
+
+
+        //update account
+        $scope.updateAcc = updateAcc;
+        function updateAcc() {
+            if (angular.equals($scope.psw, $scope.rePsw) == true) {
+                //notificationService.displaySuccess('Done');
+                var registerData = {
+                    FullName: $scope.accEdt.FullName,
+                    BirthDay: $scope.accEdt.BirthDay,
+                    UserName: $scope.accEdt.UserName,
+                    PhoneNumber: $scope.accEdt.PhoneNumber,
+                    Password: $scope.psw,
+                    Address: $scope.accEdt.Address,
+                    Roles: [$scope.accEdt.role.Name]
+                }
+                apiService.post('/api/user/add', registerData, function (result) {
+                    notificationService.displaySuccess('Thêm mới tài khoản thành công!');
+                }, function () {
+                    notificationService.displayError('Tạo mới tài khoản không thành công !');
+                });
+            } else {
+                notificationService.displayError('Mật khẩu nhập lại không khớp !');
+            }
+        }
+
     }
 })(angular.module('SmartOrder.accounts'));
