c15760bdd73e6d4b93eb30c97fdda2b3cc4c28e1
Chử Hoàng Anh <kukuanhanh@gmail.com>
2019-08-31 00:58:00 +0700

    update chart in home

--- SmartOrder/app/components/home/homeController.js
+++ SmartOrder/app/components/home/homeController.js
@@ -18,6 +18,8 @@
             color: $scope.colors
         };
 
+        getRevenueByday(1);
+
         var optionArr = new Array();
         function tmp(key, val) {
             this.key = key;
@@ -33,10 +35,6 @@
 
         //get history display in home page
         function getHistory() {
-            //var config = {
-            //    params: {
-            //    }
-            //}
             apiService.get('/api/history/getlast7day', null, function (result) {
                 $scope.history = result.data;
             }, function () {
@@ -59,7 +57,7 @@
             //}
             apiService.get('/api/dish/gettophot', null, function (result) {
                 if (result.data == null) {
-                   //data is empty it mean is not have any top hot dish
+                    //data is empty it mean is not have any top hot dish
                 } else {
                     $scope.hotDishes = result.data.tophot;
                 }
@@ -70,31 +68,54 @@
         getTopHotDish();
 
         //get default data in recently 7 days
-        function getRevenue7day() {
+        function getRevenueByday(ops) {
             var fdate = new Date();
             var tDate = new Date(fdate);
+            var drawID = 1;
 
-            tDate.setDate(tDate.getDate() - 7);
-
-
+            if (ops == 1) {
+                drawID = 1;
+                document.getElementById("dvCanvas").style.display = "block";
+                document.getElementById("dvCanvas2").style.display = "none";
+                document.getElementById("dvCanvas3").style.display = "none";
+                tDate.setDate(tDate.getDate() - 7);
+                $scope.title = "7 ngày gần đây";
+            } else {
+                drawID = 2;
+                document.getElementById("dvCanvas").style.display = "none";
+                document.getElementById("dvCanvas2").style.display = "block";
+                document.getElementById("dvCanvas3").style.display = "none";
+                tDate.setDate(tDate.getDate() - 30);
+                $scope.title = "30 ngày gần đây";
+            }
+            
             var config = {
-                params: {                    
+                params: {
                     fromDate: tDate,
                     toDate: fdate
                 }
             }
-
             apiService.get('/api/bill/getrevenue', config, function (result) {
-
-                $scope.title = "7 ngày gần đây";
+                var k = 0;
+                $scope.revenues.labels = [];
+                $scope.revenues.data = []
                 for (i = 0; i < result.data.length; i++) {
                     var getDate = new Date(result.data[i].Date);
                     var str = getDate.getDate() + '/' + getDate.getMonth();
-                    $scope.label.push(str);
-                    $scope.data.push(result.data[i].Revenue);
+                    if ($scope.revenues.labels[k] == null) {
+                        $scope.revenues.labels.push(str);
+                        $scope.revenues.data.push(result.data[i].Revenue);
+                    } else {
+                        if (str == $scope.revenues.labels[k]) {
+                            $scope.revenues.data[k] += result.data[i].Revenue;
+                        } else {
+                            $scope.revenues.labels.push(str);
+                            $scope.revenues.data.push(result.data[i].Revenue);
+                            k++;
+                        }
+                    }
                 }
-                drawChart();
-
+                drawChart(drawID);
             }, function () {
                 //notificationService.displayError('Rất tiếc đã sảy ra lỗi trong quá trình tải danh sách!');
             });
@@ -103,79 +124,65 @@
         //catch event user select box change
         $scope.selectEvent = function () {
             if ($scope.userOption == 1) {
-                getRevenue7day();
-            } else if ($scope.userOption == 2) {
-                fdate = new Date();
-                tDate = new Date(fdate);
-
-                tDate.setDate(tDate.getDate() - 30);
-
-                console.log(fdate);
-                console.log(tDate);
-
-                var config = {
-                    params: {
-                        fromDate: fdate,
-                        toDate: tDate
-                    }
-                }
-                apiService.get('/api/bill/getrevenuebymonth', config, function (result) {
-                    $scope.bills = result.data;
-                    $scope.title = "30 ngày gần đây";
-                    if ($scope.bills.length === 0) {
-                        //notificationService.displayWarning('Danh sách trống !');
-                    } else {
-
-                    }
-                }, function () {
-                    //notificationService.displayError('Rất tiếc đã sảy ra lỗi trong quá trình tải danh sách!');
-                });
+                getRevenueByday(1);
+            } else if ($scope.userOption == 2) {     
+                getRevenueByday(2);                   
             } else if ($scope.userOption === 3) {
                 getRevenuesInYear();
             }
         };
 
-        function formatRevenuesData(data) {
-            var a = data.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-            return a;
-        }
+        //function formatRevenuesData(data) {
+        //    var a = data.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+        //    return a;
+        //}
 
         function getRevenuesInYear() {
-            fdate = new Date();
-            tDate = new Date(fdate);
-            //fdate.setMonth(tDate.getMonth() - 12);
+            var fMonth = new Date();
+            var tMonth = new Date(fMonth);
+            //fMonth.setMonth(tMonth.getMonth() - 12);
 
-            var fromDate = fdate.getFullYear() + "-" + 01 + "-" + 01;
-            var toDate = tDate.getFullYear() + "-" + (tDate.getMonth() + 2) + "-" + tDate.getDate();
+            document.getElementById("dvCanvas").style.display = "none";
+            document.getElementById("dvCanvas2").style.display = "none";
+            document.getElementById("dvCanvas3").style.display = "block";
+
+            var fromDate = fMonth.getFullYear() + "-" + 01 + "-" + 01;
+            var toDate = tMonth.getFullYear() + "-" + (tMonth.getMonth() + 1) + "-" + 30;
             var url = "/api/bill/getrevenuebymonth?fromDate=" + fromDate + "&toDate=" + toDate;
-
+            $scope.revenues.data = [];
+            $scope.revenues.labels = [];
             apiService.get(url, null, function (result) {
-
                 $scope.title = "Trong năm nay";
                 var k = 0;
                 for (i = 0; i < result.data.length; i++) {
                     var month = 'Tháng ' + result.data[i].Month
-                    $scope.label.push(month);
-                    $scope.data.push(result.data[i].Revenue);
+                    $scope.revenues.labels.push(month);
+                    $scope.revenues.data.push(result.data[i].Revenue);
                     //$scope.colors.push($scope.libColor[k]);
                     //k++;
                     //if (k == 9) {
                     //    k = 0;
                     //}
                 }
-                drawChart();
-
+                drawChart(3);
             }, function () {
                 notificationService.displayError('Rất tiếc đã sảy ra lỗi trong quá trình tải danh sách!');
             });
         }
-        getRevenuesInYear();
+        //getRevenuesInYear();
 
-        function drawChart() {
-            var ctx = document.getElementById("dvCanvas").getContext('2d');            
-          
+        function drawChart(ops) {
+            var canvasID = "";
+            if (ops == 1) {
+                canvasID = "dvCanvas";
+            } else if (ops == 2) {
+                canvasID = "dvCanvas2";
+            } else {
+                canvasID = "dvCanvas3";
+            }
+            var ctx = document.getElementById(canvasID).getContext('2d');
             var myChart = new Chart(ctx, {
-                type: 'line',
+                type: 'bar',
                 data: {
                     datasets: [{
                         data: $scope.revenues.data,
@@ -185,7 +192,32 @@
                     labels: $scope.revenues.labels,
                 },
                 options: {
+                    tooltips: {
+                        callbacks: {
+                            label: function (tooltipItem, data) {
+                                return "₫ " + Number(tooltipItem.yLabel).toFixed(0).replace(/./g, function (c, i, a) {
+                                    return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
+                                });
+                            }
+                        }
+                    },
                     responsive: true,
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: false,
+                                labelString: 'VNĐ'
+                            },
+
+                               ticks: {
+                                // Include a dollar sign in the ticks
+                                callback: function (value, index, values) {
+                                    return '' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                                }
+                            }
+
+                        }]
+                    },
                     title: {
                         display: true,
                         text: $scope.title,
@@ -195,8 +227,6 @@
                 }
             });
             myChart.update();
-
         }
-      
     };
 })(angular.module('SmartOrder'));
