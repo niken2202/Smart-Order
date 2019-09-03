eb45f4f7314f32d9be7cd09e4284f41be31c7d26
Le Trong Thang <niken2202@gmail.com>
2019-09-04 00:07:39 +0700

    update cartdetail

--- Data/Migrations/Configuration.cs
+++ Data/Migrations/Configuration.cs
@@ -15,29 +15,29 @@ namespace Data.Migrations
 
         protected override void Seed(Data.SmartOrderContext context)
         {
-            //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new SmartOrderContext()));
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new SmartOrderContext()));
 
-            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new SmartOrderContext()));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new SmartOrderContext()));
 
-            //var user = new ApplicationUser()
-            //{
-            //    UserName = "thang",
-            //    Email = "hahaa.hahaha@gmail.com",
-            //    EmailConfirmed = true,
-            //    BirthDay = DateTime.Now,
-            //    FullName = "Le Trong Thang",
+            var user = new ApplicationUser()
+            {
+                UserName = "thang",
+                Email = "hahaa.hahaha@gmail.com",
+                EmailConfirmed = true,
+                BirthDay = DateTime.Now,
+                FullName = "Le Trong Thang",
 
-            //};
-            //manager.Create(user, "123456");
-            //if (!roleManager.Roles.Any())
-            //{
-            //    roleManager.Create(new IdentityRole { Name = "Admin" });
-            //    roleManager.Create(new IdentityRole { Name = "User" });
-            //}
+            };
+            manager.Create(user, "123456");
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
 
-            //var adminUser = manager.FindByName("test");
+            var adminUser = manager.FindByName("thang");
 
-            //manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
 
         }
     }
